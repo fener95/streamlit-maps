8380fe390e3c6c8a54c6b59d0dab444804faab54
Federico Neri <nerifederico1995@gmail.com>
2024-08-13 22:55:41 +0200

    labels

--- pages/7_Raster.py
+++ pages/7_Raster.py
@@ -21,7 +21,7 @@ with st.expander("See source code"):
         before = "https://github.com/fener95/tiffs-repo/raw/main/img23_true_color.tif"
         after = "https://github.com/fener95/tiffs-repo/raw/main/img24_true_color.tif"
         m.split_map(
-            left_layer=before, right_layer=after, left_label = "one day July 2023", right_label="one day july 2024",
+            left_layer=before, right_layer=after, left_label = "day of July 2023", right_label="day of july 2024",
         )
 
 m.to_streamlit(height=700)
